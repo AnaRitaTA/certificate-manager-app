adb956e0cce8fccd006a9754a035ce593da96a94
Ana Rita <115116313+AnaRitaTA@users.noreply.github.com>
2025-07-25 13:05:12 +0100

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import CertificateManager from './certificate_manager';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<CertificateManager />);
